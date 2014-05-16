c19d24f9443bfb6a42d17fecc75bf709315f85e4
William Osler <william@oslers.us>
2014-05-16 10:04:44 -0400

    Move files around and create Rakefile
    Since Weechat can be picky about requires stuff, we just have a rake
    task that cats everything into one big script to be loaded into weechat.
    Hacky, but it works.
    
    This commit introduces the /moon command, which when given the date will
    return the approximate illumination of the moon for that date.

---
+++ Rakefile
@@ -0,0 +1,3 @@
+task :default do
+  sh "cat sunset.rb moonphase.rb script.rb > fullmoon.rb"
+end
---
+++ script.rb
@@ -0,0 +1,81 @@
+# Weechat Fullmoon script
+#
+# This is a simple ruby script for weechat that can perform certain actions when
+# there's a full moon.
+#
+# Author::    William Osler (mailto:{firstname}@{lastname}s.us)
+# License::   MIT
+
+$script_name = "fullmoon"
+
+$options = { latitude: '0', longitude: '0', moon_nick: "My2SpookyNick",
+  moon_trigger: '0.995', action: 'does a thing', channels:
+  "freenode.#foo,esper.#bar"}
+
+def weechat_init
+  Weechat.register($script_name, "William Osler <{firstname}@{lastname}s.us>",
+                   "0.1", "MIT", "Performs certain actions during a full moon",
+                   "shutdown", "")
+
+  # Initialize config
+  $options.each do |key, value|
+    if Weechat.config_is_set_plugin(key.to_s) then
+      $options[key] = Weechat.config_get_plugin(key.to_s)
+    else
+      Weechat.config_set_plugin(key.to_s, value)
+    end
+  end
+
+  # Listen for config changes
+  Weechat.hook_config("plugins.var.ruby." + $script_name + ".*", "config_cb", "")
+
+  # Our commands
+  Weechat.hook_command("moon",
+                       "Returns the percentage of the moon illuminated on a given date",
+                       "[date]",
+                       "a valid ruby date like 2014-12-25",
+                       "",
+                       "moon_command_cb",
+                       "")
+
+  return Weechat::WEECHAT_RC_OK
+end
+
+def shutdown
+  Weechat.print("", "Fullmoon plugin shutting down.")
+  return Weechat::WEECHAT_RC_OK
+end
+
+def config_cb(data, option, value)
+  # load options
+  $options.each_key do |key|
+    if Weechat.config_is_set_plugin(key.to_s) then
+      $options[key] = Weechat.config_get_plugin(key.to_s)
+    end
+  end
+
+  return Weechat::WEECHAT_RC_OK
+end
+
+def moon_command_cb(data, buffer, args)
+  if args.nil? || args.empty?
+    Weechat.print(buffer, "You must specify a date.")
+    return Weechat::WEECHAT_RC_OK
+  end
+
+  datestr = args.split(" ")[0]
+
+  begin
+    day = Date.parse(datestr)
+    illum = MoonPhase.moon_illumination_for(day) * 100
+    Weechat.print(buffer, "Illumination for #{day.iso8601}: #{illum}")
+  rescue ArgumentError => e
+    if (e.message == "invalid date") then
+      Weechat.print(buffer, "Invalid date.")
+    else
+      raise e
+    end
+  end
+
+  return Weechat::WEECHAT_RC_OK
+end
